5dee34f013a5d481699fcdc086c59282abb99ecf
Shawn Mathew <shawnmathew14@gmail.com>
2026-01-28 11:14:32 -0500

    Adding LLM Reasoner for Gemini Integration only when necessary instead of combining the agent framework all together

--- .venv/nodes/llm_reasoner.py
+++ .venv/nodes/llm_reasoner.py
@@ -2,3 +2,19 @@ import os
 from google import genai
 
 
+client = genai.Client(api_key=os.getenv("GENAI_API_KEY"))
+
+def llm_reasoner(state: dict) -> dict:
+    prompt = f"""
+    AWS RISK: {state.get("risk")}
+    Recommendation: {state.get("recommendation")}
+    """
+
+    given_response = client.models.generate_content(
+        model = "gemini-2.0-flash",
+        contents = prompt
+    )
+    
+    state["llm_rundown"] = given_response.text
+    return state
+
